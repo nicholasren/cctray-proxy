4247b6d2fec72b8664c321c46a7b63328459d45f
Xiaojun REN <xren@atlassian.com>
2024-11-03 09:45:21 +1100

    Set header

--- src/proxy.rs
+++ src/proxy.rs
@@ -1,15 +1,16 @@
 use axum::{routing::get, Router};
+use axum::response::IntoResponse;
 use crate::{config, pipeline_fetcher};
 
 pub async fn start(addr: &str) {
     let app = Router::new()
-        .route("/", get(handler));
+        .route("/cctray.xml", get(handler));
 
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn handler() -> String {
+async fn handler() -> impl IntoResponse {
     let configs = config::load();
     let config = configs.first().unwrap();
     let pipelines = pipeline_fetcher::fetch(&config.id, &config.bearer_token).await;
@@ -18,7 +19,8 @@ async fn handler() -> String {
         .map(|pipeline| pipeline.to_xml())
         .collect::<Vec<_>>().join("\n");
 
-    format!(r#"<Projects>
-    {}
-    </Projects>"#, pipelines_in_xml)
+    let body = format!("<Projects>{}</Projects>", pipelines_in_xml);
+
+    let headers = [("Content-Type", "application/xml; charset=UTF-8")];
+    (headers, body)
 }
