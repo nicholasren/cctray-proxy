c972c75c8d76f5ab1880173989f6acabf9670a4b
Xiaojun REN <xren@atlassian.com>
2024-11-02 13:06:02 +1100

    Accept config file path via cli arg.

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,17 @@ mod config;
 mod pipeline;
 mod json;
 mod proxy;
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    config_path: std::path::PathBuf,
+}
 
 #[tokio::main]
 async fn main() {
+    let args = Cli::parse();
+    println!("Using config path: {:?}", args.config_path);
+
     proxy::start().await;
 }
